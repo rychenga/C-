46308e62b638e3d5ea1661973772618a2fa14da1
rychenga <s813031ranyi@yahoo.com.tw>
2016-05-15 14:55:55 +0800

    Create Actini.cs

---
+++ Actini.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Actini
+{
+    class Program
+    {
+        //import kernel32 for writer ini.
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+        //import kernel32 for read ini.
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString,int nSize, string lpFileName);
+
+        public static void WriteIni(string section, string key, string input, string Path)
+        {
+            WritePrivateProfileString(section, key, input, Path);
+
+        }
+        public static string ReadIni(string section, string key,string Path)
+        {
+            string sRec;
+            StringBuilder record = new StringBuilder();
+            GetPrivateProfileString(section,key, "", record, 255, Path);
+            sRec = record.ToString();
+            return sRec;
+        }
+    }
+}
