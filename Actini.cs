6d6f26609fd22c3a1231dd6c049b3b63bc0f8496
rychenga <s813031ranyi@yahoo.com.tw>
2016-05-15 14:58:28 +0800

    Update Actini.cs

--- Actini.cs
+++ Actini.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices; //DllImport need
 
 namespace Actini
 {
