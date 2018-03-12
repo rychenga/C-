e2884d47ef30864fb5f6040ee571f5ee813c921e
rychenga <s813031ranyi@yahoo.com.tw>
2018-03-12 17:02:08 +0800

    Create 20180312_demo.cs

---
+++ 20180312_demo.cs
@@ -0,0 +1,45 @@
+//first include IO.dll & Threading.dll
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //宣告bytes 陣列
+            List<byte> _lInput = new List<byte>();
+            //宣告PLC
+            //_comPLC = new SerialPort("COM4", 115200, Parity.Even, 8, StopBits.Two);
+            AGVController.AAGVModbus _comPLC = new AGVController.AAGVModbus(4, 115200, Parity.Even, 8, StopBits.Two);
+
+            try
+            {
+
+                Console.WriteLine("Input Station:");
+                int _iStation = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input Function:");
+                int _iFunction = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input Address:");
+                int _iAddress = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input data:");
+                int _iData = Convert.ToInt32(Console.ReadLine());
+                    
+                _comPLC.Open();
+
+                //_comPLC.WriteRtuCrc(01,03,000000,01);
+                _comPLC.WriteRtuCrc(_iStation, _iFunction, _iAddress, _iData);
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
